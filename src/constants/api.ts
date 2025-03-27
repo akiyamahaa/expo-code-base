d4ad656f3b568ed55a6c560704fe38ef7ad8c178
Quang Akiyama <quanganhcf2@gmail.com>
2025-03-27 20:48:23 +0700

    feat: remove redundant file

--- src/constants/api.ts
+++
@@ -1,6 +0,0 @@
-const BASE_URL = ''
-
-export const LOGIN_API = `${BASE_URL}/auth/login`
-export const REGISTER_API = `${BASE_URL}/auth/register`
-export const ME_API = `${BASE_URL}/auth/me`
-export const LOGOUT_API = `${BASE_URL}/auth/logout`
