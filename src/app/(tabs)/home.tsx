b4ba8dd85ce5df26c0405bd1a4e144a963f44668
Quang Akiyama <quanganhcf2@gmail.com>
2025-04-02 16:23:52 +0700

    feat: setup basic form and test auth flow

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
 import React from 'react'
+import { useAuthContext } from '@/context/AuthProvider'
 
 const Home = () => {
+  const { signOut } = useAuthContext()
+  const { user } = useAuthContext()
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 mt-12">
+      <Text>Hello, {user?.name}</Text>
+      <Pressable onPress={signOut} className="bg-red-500 px-6 py-3 rounded w-40">
+        <Text className="text-white font-semibold">Sign Out</Text>
+      </Pressable>
     </View>
   )
 }
